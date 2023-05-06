e97edd26a84ff888dc6b22f315ba7f6961e7d62a
lakshay <lakshayakalra29@gmail.com>
2023-05-06 19:33:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const port = process.env.PORT || 8000
+
+
+function dbconnect(){
+    mongoose.connect("mongodb://127.0.0.1:27017/monoogseConnect").then(()=>{console.log("connected");}).catch((err)=>{console.log(err.message);})
+}
+
+dbconnect();
+
+app.get("/", function(req, res){
+    res.send("Hello, World!")
+})
+
+//schema or models
+const uSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password:String,
+},{versionKey:false})
+
+const User = new mongoose.model("User",uSchema)
+
+//data
+const user = new User({
+    name: "Lakshaya",
+    email: "Lakshayakalra29@gmail.com",
+    password: "password"
+})
+user.save();
+
+
+app.listen(port,function(){
+    console.log("Server started "+ port)
+})
