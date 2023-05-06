37f8cf0fb98f95c1b7203c1f215e08eefcc72fe9
lakshay <lakshayakalra29@gmail.com>
2023-05-06 19:51:00 +0530

    changed db name for local

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000
 
 
 function dbconnect(){
-    mongoose.connect("mongodb://127.0.0.1:27017/monoogseConnect").then(()=>{console.log("connected");}).catch((err)=>{console.log(err.message);})
+    mongoose.connect("mongodb://127.0.0.1:27017/mongooseConnect").then(()=>{console.log("connected");}).catch((err)=>{console.log(err.message);})
 }
 
 dbconnect();
